c1b884f60275ccf3770624a7c95d9d5a0618240d
‪Mohammed Abbas‬‏ <75201675+mohammedabbas2000@users.noreply.github.com>
2022-09-27 01:10:30 +0300

    Update HomeW3.cpp

--- HomeWork3/HomeW3.cpp
+++ HomeWork3/HomeW3.cpp
@@ -7,14 +7,14 @@ int main()
 {
 	BST *t = (BST*)malloc(sizeof(BST));
 	t->root = NULL;
-	insert(t, 208172569);
-	insert(t, 323894741);
+	insert(t, 208170009);
+	insert(t, 323890001);
 	insert(t, 222222222);
 	insert(t, 333333333);
-	updateExamGrade(t, 208172569, 70);
-	updateMidtermGrade(t, 208172569, 100);
-	updateExamGrade(t, 323894741, 60);
-	updateMidtermGrade(t, 323894741, 100);
+	updateExamGrade(t, 208170009, 70);
+	updateMidtermGrade(t, 208170009, 100);
+	updateExamGrade(t, 323890001, 60);
+	updateMidtermGrade(t, 323890001, 100);
 	updateExamGrade(t, 333333333, 100);
 	updateMidtermGrade(t, 333333333, 85);
 	updateExamGrade(t, 222222222, 70);
